c604c312035577beef2aaaa836f4399480ecbde0
tosy <tosy@163.com>
2018-11-25 12:51:37 +0800

    1
    1

--- server/hello/helloimpl/hello.go
+++ server/hello/helloimpl/hello.go
@@ -7,6 +7,10 @@ import (
 	"time"
 
 	pb "github.com/golangstudy0/server/hello/hello"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+
 )
 
 // server is used to implement hello.GreeterServer.
@@ -18,10 +22,33 @@ var (
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+
+	db, _ = sql.Open("mysql", "root:root@/user")
 )
+
+
 
 // SayHello implements hello.GreeterServer
 func (s *Hello) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+
+	//插入数据
+	stmt, err := db.Prepare("INSERT userinfo SET name=?,age=?")
+	res, err := stmt.Exec("testname", 1)
+	if nil != res {
+		nr ,_:= res.RowsAffected()
+		fmt.Println("rows affect : " ,nr )
+	}
+
+	rows, err := db.Query("SELECT * FROM userinfo")
+	for rows.Next() {
+		var id int
+		var name string
+		var age int
+		err = rows.Scan(&id, &name, &age)
+		fmt.Println(id)
+		fmt.Println(name)
+		fmt.Println(age)
+	}
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
