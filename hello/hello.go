51d522c9b9e41adeea1792270af96c79bf5654fd
tosy <tosy@163.com>
2018-11-29 16:27:45 +0800

    1
    1

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
 type world interface {
@@ -18,13 +15,27 @@ type hello interface {
 type test struct {
 	a	int
 }
-func (*test) Hi(){
-	fmt.Println("hhe")
+
+type test2 struct {
+	a	int
 }
 
-//var _ hello = (*test)(nil)
+func (*test) Hi(){
+	fmt.Println("1111")
+}
+
+func (*test2) Hi(){
+	fmt.Println("2222")
+}
+
+var _ hello = (*test)(nil)		//编译器校验  nil指针转结构体指针--赋值--interface。校验结构体是否实现了interface
 
 func main() {
+	tA := test{a:1}
+	iA := (hello)(&tA)
+	tC := iA.(*test2)			//panic		指针的结构体name不同于转换后的结构体name，接口实现可能不同！
+	tC.Hi()
+
 	t1 := test{a : 1}
 	//t1.Hi()
 	t2 := (world)(&t1)
@@ -33,13 +44,13 @@ func main() {
 	}
 	fmt.Println("hello")
 
-	r, e := http.Get("https://www.tosylab.com")
-	if e != nil {
-		fmt.Println(e)
-	}
-
-	io.Copy(os.Stdout, r.Body)
-	if err := r.Body.Close(); err != nil {
-		fmt.Println(err)
-	}
+	//r, e := http.Get("https://www.tosylab.com")
+	//if e != nil {
+	//	fmt.Println(e)
+	//}
+	//
+	//io.Copy(os.Stdout, r.Body)
+	//if err := r.Body.Close(); err != nil {
+	//	fmt.Println(err)
+	//}
 }
