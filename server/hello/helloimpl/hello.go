c391c6a54451a407de48d38915de9951d06904d8
tosy <tosy@163.com>
2018-12-03 11:26:14 +0800

    1
    1

--- server/hello/helloimpl/hello.go
+++ server/hello/helloimpl/hello.go
@@ -13,6 +13,19 @@ import (
 
 )
 
+//create database user
+/**
+create table userinfo (
+	`uid` bigint(20) NOT NULL AUTO_INCREMENT comment '用户id' ,
+	`name` varchar(120) NOT NULL comment '用户名' ,
+	`age` int(11) NOT NULL  comment '年龄',
+	`create_time` timestamp NOT NULL  DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '创建时间',
+
+	PRIMARY KEY (`uid`),
+	KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 comment '用户表';
+ */
+
 // server is used to implement hello.GreeterServer.
 type Hello struct{}
 
